40e497069a7995771d799181fe2d07af648dae10
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2024-04-01 07:44:10 -0600

    Just a loading message, will change later for a most stylish one

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Router, ActivatedRoute, RouterOutlet } from '@angular/router';
 import { LoadingService } from '../loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
